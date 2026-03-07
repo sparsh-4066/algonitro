61c9ba4640dc57e2fcfa587021ba547235ef0827
sparsh-4066 <sparshuser1234@gmail.com>
2026-03-07 21:18:01 +0530

    fix api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://algonitro-backend.onrender.com/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 export default API;
